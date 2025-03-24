a6bd316017ffe4f1dc74e73908e9535f1598ebd3
PhamHoangToan <phamhoangtoan13@gmail.com>
2025-03-24 10:11:50 +0700

    update routes product

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,15 +1,15 @@
 const express=require("express");
 const{getProduct, getProductById, createProduct, updateProduct, deleteProduct,searchProducts}=require("../controllers/productController");
 const router=express.Router();
-
+router.get('/search', (req, res, next) => {
+  console.log('Hit vào ROUTE /search');
+  next(); // chuyển tiếp đến controller
+}, searchProducts);
 router.get("/", getProduct);
 router.get("/:id", getProductById);
 router.post("/", createProduct);
 router.put("/:id", updateProduct);
 router.delete("/:id", deleteProduct);
-router.get('/search', (req, res, next) => {
-    console.log('Hit vào ROUTE /search');
-    next(); // chuyển tiếp đến controller
-  }, searchProducts);
+
 
 module.exports=router;
